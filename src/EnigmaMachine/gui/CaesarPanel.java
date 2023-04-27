c6137b118e32535b773a591e33a2fb6d2283dc02
MehradHajati <79949480+MehradHajati@users.noreply.github.com>
2023-04-26 22:04:33 -0300

    completed caesarPanel

--- src/EnigmaMachine/gui/CaesarPanel.java
+++ src/EnigmaMachine/gui/CaesarPanel.java
@@ -1,16 +1,18 @@
 package EnigmaMachine.gui;
 
 import EnigmaMachine.ViewManager;
-import java.awt.BorderLayout;
+import EnigmaMachine.EnigmaMachine;
+import EnigmaMachine.Dialogs;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.font.TextAttribute;
 import java.util.Map;
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -22,8 +24,9 @@ import javax.swing.JTextField;
 public class CaesarPanel extends JPanel{
     
     private ViewManager viewManager;
-    private JLabel cipherName, cipherIntro, enterKey;
-    private JTextField key;
+    private JLabel cipherName, cipherIntro, enterKey, enterText, output;
+    private JTextField key, input;
+    private JButton encryptButton, decryptButton;
     private GridBagConstraints constraints;
     
     public CaesarPanel(ViewManager view){
@@ -48,54 +51,177 @@ public class CaesarPanel extends JPanel{
         attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
         cipherName.setFont(font.deriveFont(attributes));
         
+        //Creating the label which will explain how the cipher works and changing its font
         cipherIntro = new JLabel("<html>Assume each letter in the alphabet is numbered from 0-25.<br> "
                 + "In the Ceasar Cipher, using one integer keys A, the data is encrypted by shifting the letters by A in the alphabet and then modding by 26.<html>");
         cipherIntro.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 10));
         
+        // creating the label that will tell the user to type in their cipher or plain text and changing its font
+        enterText = new JLabel("Enter your Ciphertext or Plaintext:");
+        enterText.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 10));
+        
+        // creating the text field that the user will type in their cipher of plain text
+        input = new JTextField(20);
+        input.addKeyListener(new KeyAdapter(){});
+        
+        //creating the label that will tell the user to type in their key and changing its font
         enterKey = new JLabel("Enter your key:");
         enterKey.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 10));
         
+        // creating the text field that the user will tyep in their key into
         key = new JTextField(10);
-        key.addKeyListener(new KeyAdapter(){
+        key.addKeyListener(new KeyAdapter(){});
+        
+        //creating the encrypt button and adding its actionListener
+        encryptButton = new JButton("Encrypt");
+        encryptButton.addActionListener(new ActionListener(){
             @Override
-            public void keyPressed(KeyEvent ke) {
-                String value = key.getText();
-                int l = value.length();
-                if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') {
-                    key.setEditable(true);
-                } 
-                else{
-                    key.setEditable(false);
-                }
+            public void actionPerformed(ActionEvent e){
+                encrypt();
             }
         });
+        
+        //creating the decrypt button and adding its actionListener
+        decryptButton = new JButton("Decrypt");
+        decryptButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                decrypt();
+            }
+        });
+        
+        // creating the label which will show the user the output
+        output = new JLabel("Your Output");
+        output.setFont(new Font(Font.MONOSPACED, Font.BOLD, 13));
+               
         
     }
     
     private void addObjects(){
+        // adding the cipherName to the panel, using constrains and the grid system it comes with
         constraints.anchor = GridBagConstraints.PAGE_START;
         constraints.gridx = 0;
         constraints.gridy = 0;
         add(cipherName, constraints);
         
+        // adding the cipher explaination or intro to the panel
         constraints.insets = new Insets(10,0,0,0);
         constraints.anchor = GridBagConstraints.CENTER;
         constraints.gridx = 0;
         constraints.gridy = 1;
         add(cipherIntro, constraints);
         
+        // addiong the enterText label to the panel
         constraints.insets = new Insets(10,0,0,0);
         constraints.anchor = GridBagConstraints.LINE_START;
         constraints.gridx = 0;
         constraints.gridy = 2;
+        add(enterText, constraints);
+        
+        // adding the input text field to the panel
+        constraints.insets = new Insets(10,0,0,0);
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        add(input, constraints);
+        
+        // adding the enterKey label to the panel
+        constraints.insets = new Insets(10,0,0,0);
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.gridx = 0;
+        constraints.gridy = 3;
         add(enterKey, constraints);
         
+        // adding the key text field to the panel
         constraints.insets = new Insets(10,0,0,0);
         constraints.anchor = GridBagConstraints.LINE_START;
         constraints.gridx=1;
-        constraints.gridy=2;
+        constraints.gridy=3;
         add(key, constraints);
         
+        // adding the encryptButton to the panel
+        constraints.insets = new Insets(10,0,0,0);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridx=0;
+        constraints.gridy=4;
+        add(encryptButton, constraints);
+        
+        // adding the decryptButton to the panel
+        constraints.insets = new Insets(10,0,0,0);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridx=1;
+        constraints.gridy=4;
+        add(decryptButton, constraints);
+        
+        // adding the output label to the panel
+        constraints.insets = new Insets(10,0,0,0);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridx=0;
+        constraints.gridy=5;
+        add(output, constraints);
     }
     
+    /*
+    Method to decrypt
+    */
+    private void decrypt(){
+        if(checkKey()){
+            if(checkInput()){
+                String answer = EnigmaMachine.decryptCaesar(input.getText(), Integer.parseInt(key.getText()));
+                output.setText(answer);
+            }
+            else{
+                Dialogs.error(viewManager, "The text you have entered is invalid, ensure it only contains the English alphabet", "Invalid Text");
+            }
+        }
+        else{
+            Dialogs.error(viewManager, "The key you have entered is invalid, ensure it is a number", "Invalid Key");
+        }
+    }
+    
+    /*
+    Method to encrypt
+    */
+    private void encrypt(){
+        if(checkKey()){
+            if(checkInput()){
+                String answer = EnigmaMachine.encryptCaesar(input.getText(), Integer.parseInt(key.getText()));
+                output.setText(answer);
+            }
+            else{
+                Dialogs.error(viewManager, "The text you have entered is invalid, ensure it contains only the English alphabet", "Invalid Text");
+            }
+        }
+        else{
+            Dialogs.error(viewManager, "The key you have entered is invalid, ensure it is a number", "Invalid Key");
+        }
+    }
+    
+    /*
+    Method to check whether key is valid
+    */
+    private boolean checkKey(){
+        String value = key.getText();
+        if(value == null){
+            return false;
+        }
+        if(!value.matches("[0-9]+")){
+            return false;
+        }
+        return true;
+    }
+    
+    /*
+    Method to check whether input is valid
+    */
+    private boolean checkInput(){
+        String text = input.getText();
+        if(text == null){
+            return false;
+        }
+        if(!text.matches("[a-zA-Z]+")){
+            return false;
+        }
+        return true;
+    }
 }
